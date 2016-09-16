2911c4285d0da9e53c27affd2fbe7dfacc1e043c
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-09-16 17:44:12 +0100

    Code has been refactored into functions

--- getting_to_philosophy.py
+++ getting_to_philosophy.py
@@ -22,28 +22,101 @@ def getting_to_philosophy():
     """
 
     # Fetch the desired page and its contents.
-    response = requests.get(base_url + departure)
+    response = get_page(departure)
     # Turn the content into soup using the html parser.
-    soup = BeautifulSoup(response.content, "html.parser")
+    soup = soupify_page(response.content)
     # Find the section of the page that is just the article.
-    article = soup.find("div", id="bodyContent")
-    # Find all of the paragraphs within the article as these contain the
-    # anchors we are interested in.
-    paragraphs = article.find_all("p")
+    article = get_article(soup)
+    # Find all of the paragraphs within the article
+    paragraphs = get_paragraphs(article)
     # Find all of the anchor tags within the paragraphs found above.
-    anchors = [paragraph.find_all("a") for paragraph in paragraphs]
-    # Get all of links excluding upper case links and coordinates.
+    anchors = get_anchors(paragraphs)
+    # Get all of the desired links.
+    links = get_links(anchors)
+    # Print the links contained within the page that and acceptable.
+    print(links)
+
+
+def get_page(url):
+    """ Make a single request for wikipedia article to be returned.
+
+    Args:
+        url: A string defining the url of the wanted wikipedia article.
+
+    Returns:
+        response: A requests object containing the content of the http request.
+    """
+    return requests.get(base_url + url)
+
+
+def soupify_page(content):
+    """ Create and returns and BeautifulSoup object using the supplied article.
+
+    Args:
+        content: Container with the content of retrieved wikipedia article.
+
+    Returns:
+        soup: A BeautifulSoup object is supplied back.
+    """
+    return BeautifulSoup(content, "html.parser")
+
+
+def get_article(soup):
+    """ Get the article contained within the wikipedia page.
+
+    Args:
+        soup: BeautifulSoup object with the required wikipedia article within.
+
+    Returns:
+        article: A BeautifulSoup Tag object containing the article.
+    """
+    return soup.find("div", id="bodyContent")
+
+
+def get_paragraphs(article):
+    """ Get all of the paragraphs contained within the article.
+
+    Args:
+        article: BeautifulSoup Tag object containing the desired paragraphs.
+
+    Returns:
+        paragraphs: A list of paragraphs (p tags) are sent back.
+    """
+    return article.find_all("p")
+
+
+def get_anchors(paragraphs):
+    """ Get all of the anchors within the article's paragraph.
+
+    Args:
+        paragraphs: List of paragraphs (p tags) containing anchors (a tags)
+
+    Returns:
+        anchors: A list all anchors found within paragraphs.
+    """
+    return [paragraph.find_all("a") for paragraph in paragraphs]
+
+
+def get_links(anchors):
+    """ Create a list of links that meet a series of requirements.
+
+    Args:
+        anchors: List of anchors found within the current page paragraphs
+
+    Returns:
+        links: A list of links (String) found within that are acceptable.
+    """
     links = [link.get("href") for link in
              [anchor for sublist in anchors for anchor in sublist]
              if link.parent.get("id") != "coordinates" and link.text.islower()]
-    # Further exclude links if the contain unwanted elements.
+
     for link in list(links):
         for element in unwanted_elements:
             if element in link:
                 links.remove(link)
                 break
-    # Print the links contained within the page that and acceptable.
-    print(links)
+
+    return links
 
 if __name__ == "__main__":
     getting_to_philosophy()
